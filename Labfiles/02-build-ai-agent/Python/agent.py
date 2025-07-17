505022c5f25144ff02d1214a0b6ab87723060737
jcmayacu <jcmayacu@unal.edu.co>
2025-07-16 21:59:20 -0500

    ...

--- Labfiles/02-build-ai-agent/Python/agent.py
+++ Labfiles/02-build-ai-agent/Python/agent.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 from typing import Any
 from pathlib import Path
 
-
 # Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents import AgentsClient
+from azure.ai.agents.models import FilePurpose, CodeInterpreterTool, ListSortOrder, MessageRole
+
+from docx import Document
 
 
 def main(): 
@@ -19,23 +23,55 @@ def main():
 
     # Display the data to be analyzed
     script_dir = Path(__file__).parent  # Get the directory of the script
-    file_path = script_dir / 'data.txt'
+    # file_path = script_dir / 'data.txt'
+    file_path = script_dir / 'Expenses_Policy.docx'
 
-    with file_path.open('r') as file:
-        data = file.read() + "\n"
+    # with file_path.open('r') as file:
+    #     data = file.read() + "\n"
+    #     print(data)
+
+    # Para leer archivo de Word
+    if file_path.suffix == '.txt':
+        with file_path.open('r') as file:
+            data = file.read() + "\n"
+            print(data)
+    elif file_path.suffix == '.docx':
+        # Read the Word document        
+        doc = Document(file_path)
+        data = ""
+        for paragraph in doc.paragraphs:
+            data += paragraph.text + "\n"
         print(data)
 
     # Connect to the Agent client
-
+    agent_client = AgentsClient(
+        endpoint=project_endpoint,
+        credential=DefaultAzureCredential
+            (exclude_environment_credential=True,
+            exclude_managed_identity_credential=True)
+    )
+    with agent_client:
 
         # Upload the data file and create a CodeInterpreterTool
+        file = agent_client.files.upload_and_poll(
+            file_path=file_path, purpose=FilePurpose.AGENTS
+        )
+        # print(f"Uploaded {file.filename}")
 
+        code_interpreter = CodeInterpreterTool(file_ids=[file.id])
 
         # Define an agent that uses the CodeInterpreterTool
-
+        agent = agent_client.create_agent(
+            model=model_deployment,
+            name="Data Analysis Agent",
+            instructions="You are an AI agent that analyzes the data in the file that has been uploaded. Use Python to calculate statistical metrics as necessary.",
+            tools=code_interpreter.definitions,
+            tool_resources=code_interpreter.resources,
+        )
+        print(f"Using agent: {agent.name}")
 
         # Create a thread for the conversation
-
+        thread = agent_client.threads.create()
     
         # Loop until the user types 'quit'
         while True:
@@ -48,19 +84,36 @@ def main():
                 continue
 
             # Send a prompt to the agent
+            message = agent_client.messages.create(
+                thread_id=thread.id,
+                role="user",
+                content=user_prompt,
+            )
 
+            run = agent_client.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
 
             # Check the run status for failures
-
+            if run.status == "failed":
+                print(f"Run failed: {run.last_error}")
     
             # Show the latest response from the agent
-
+            last_msg = agent_client.messages.get_last_message_text_by_role(
+                thread_id=thread.id,
+                role=MessageRole.AGENT,
+            )
+            if last_msg:
+                print(f"Last Message: {last_msg.text.value}")
 
         # Get the conversation history
-    
+        # print("\nConversation Log:\n")
+        # messages = agent_client.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
+        # for message in messages:
+        #     if message.text_messages:
+        #         last_msg = message.text_messages[-1]
+        #         print(f"{message.role}: {last_msg.text.value}\n")
 
         # Clean up
-
+        agent_client.delete_agent(agent.id)
     
 
 
